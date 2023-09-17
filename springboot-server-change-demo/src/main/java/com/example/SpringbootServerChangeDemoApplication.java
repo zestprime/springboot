e437e7fb0798e7daa816db27554ae08fc2234344
zestprime <134793760+zestprime@users.noreply.github.com>
2023-09-17 20:59:27 +0530

    Spring boot server change demo

---
+++ springboot-server-change-demo/src/main/java/com/example/SpringbootServerChangeDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServerChangeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServerChangeDemoApplication.class, args);
+	}
+}
