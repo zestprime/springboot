462967964eecc0608cd7fb8a885b9ad3e364a34d
manohar427 <manohar427@gmail.com>
2025-02-02 14:16:49 +0530

    Spring boot ollama tutorial

--- Spring-ollama/src/main/java/com/test/demo/MyController.java
+++ Spring-ollama/src/main/java/com/test/demo/MyController.java
@@ -4,6 +4,7 @@ package com.test.demo;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,13 +13,9 @@ public class MyController {
 	@Autowired
 	ChatModel chatmodel;
 
-	public void setChatModel(ChatModel chatmodel) {
-		this.chatmodel = chatmodel;
-	}
-
 	@RequestMapping("/hello")
-	public String hello() {
+	public String hello(@RequestParam String name) {
 		System.out.println("hello");
-		return chatmodel.call("tell me a joke ?");
+		return chatmodel.call(name);
 	}
 }
