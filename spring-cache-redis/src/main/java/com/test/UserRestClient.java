1a1e9de224195f3e06aeb55b4c5a940ac5bfc032
manohar427 <manohar427@gmail.com>
2025-04-18 23:16:44 +0530

    Spring cache redis

---
+++ spring-cache-redis/src/main/java/com/test/UserRestClient.java
@@ -0,0 +1,32 @@
+package com.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestClient {
+
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/details")
+	public String getUser(@RequestParam Long id) {
+		return userService.getUserById(id);
+	}
+
+	@DeleteMapping("/details")
+	public String deleteUser(@RequestParam Long id) {
+		userService.evictCache(id);
+		return "Cache cleared for user " + id;
+	}
+
+	@PutMapping("/details")
+	public String updateUser(@RequestParam Long id) {
+		return userService.updateUser(id);
+	}
+}
---
+++ spring-cache-redis/src/main/java/com/test/UserService.java
@@ -0,0 +1,32 @@
+package com.test;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+	@Cacheable(value = "users", key = "#id")
+	public String getUserById(Long id) {
+		logger.info("Fetching user with ID: {}", id); // DB Call simulate
+		return "User Details of :" + id;
+	}
+
+	@CacheEvict(value = "users", key = "#id")
+	public void evictCache(Long id) {
+		logger.info("Deleting user with ID: {}", id); // DB Call simulate
+	}
+
+	@CachePut(value = "users", key = "#id")
+	public String updateUser(Long id) {
+		logger.info("Updating user ID: {}", id); // DB Call simulate
+		return "User Details Updated :" + id;
+	}
+}
