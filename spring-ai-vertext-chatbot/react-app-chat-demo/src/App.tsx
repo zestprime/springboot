4f4901313bc47579dac8e053dec2a7a8ec3656a8
Zest Prime <zestprime@gmail.com>
2025-08-16 11:55:05 +0530

    React chatbot app demo

---
+++ spring-ai-vertext-chatbot/react-app-chat-demo/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import ChatBot from "./chatbot/ChatBot";
+
+function App() {
+
+  return (
+    <>
+    
+      <div className="card">
+       <div className='zest-prime-title'>Zest Prime</div>
+         <ChatBot/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
